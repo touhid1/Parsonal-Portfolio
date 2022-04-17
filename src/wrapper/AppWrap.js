31aab54d7c337229d80284ae608493aa8d3c583c
Touhidul islam <touhidulislam066@gmail.com>
2022-04-18 00:48:48 +0600

    copyright

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2020 TOUHIDUL</p>
+          <p className="p-text">@2022 TOUHIDUL</p>
           <p className="p-text">All rights reserved</p>
         </div>
       </div>
